f956b27eaa6657d0ae37d1add9d44f75d116dda3
siddhi2218 <153412157+siddhi2218@users.noreply.github.com>
2026-03-23 15:51:30 -0500

    added sentence-transformers dependency

--- retrieval/vector_store.py
+++ retrieval/vector_store.py
@@ -2,16 +2,22 @@ from sentence_transformers import SentenceTransformer
 import faiss
 import numpy as np
 
+# Load embedding model
 model = SentenceTransformer("all-MiniLM-L6-v2")
+
 
 def create_vector_index(chunks):
 
+    # Convert text → embeddings
     embeddings = model.encode(chunks)
 
+    # Get dimension size
     dimension = embeddings.shape[1]
 
+    # Create FAISS index
     index = faiss.IndexFlatL2(dimension)
 
+    # Add embeddings to index
     index.add(np.array(embeddings))
 
     return index, embeddings
